e6277c62885aae6cda8695332373c75770e18094
Mike <mfcustance@gmail.com>
2018-12-04 21:17:44 -0500

    Final_Project

--- FInal_Presentation.py
+++ FInal_Presentation.py
@@ -5,6 +5,9 @@ def write_file(filename: str, contents: str) -> None:
         f.write(contents)
 
 
-slides = pypandoc.convert_file('Final_Project.md', to = 'dzslides', extra_args=['-s'])
+slides = pypandoc.convert_file('Final_Project.md', to = 'slidy', extra_args=['-s'])
+#slides = pypandoc.convert_file('Final_Project.md', to='revealjs', extra_args=['-s', '-V', 'revealjs-url=http://lab.hakim.se/reveal-js', '-V', 'theme=sky'])
 
 write_file('Final_Project.html', slides)
+
+
---
+++ FInal_Project.md
@@ -0,0 +1,6 @@
+# **A Word Frequency Analysis of President Trump's Most Recent Tweets**
+
+
+                By Gianna Tricola and Michael Custance
+#NEW MOTHAFUCKIN SLIDE
+    
